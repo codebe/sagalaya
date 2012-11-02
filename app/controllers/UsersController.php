91926323437b263e3be0edb3209a8896131b8f29
Mukhamad Ikhsan <mukhamad.ikhsan@kiranatama.com>
2012-11-02 16:47:50 +0700

    adding test for repository

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -2,12 +2,21 @@
 
 namespace app\controllers;
 
+use app\models\User;
+
 class UsersController extends \lithium\action\Controller {
 	
 	public $publicActions = array('index');
 	
 	public function index() {
-		die('hello');
+        $user = new User();
+        $user::test();
+        $user::testOne('one');
+        $user::testTwo('one', 'two');
+        $user::testThree('one', 'two', 'three');
+        $user::testFour('one', 'two', 'three', 'four');
+        $user::testFive('one', 'two', 'three', 'four', 'five');
+		die('end here..');
 	}
 	
 }
--- app/resources/repository/UserRepository.php
+++ app/resources/repository/UserRepository.php
@@ -3,5 +3,27 @@ namespace app\resources\repository;
 
 class UserRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function test() {
+        echo "start test..<br/>";
+    }
 
+    public function testOne($one) {
+        echo "{$one}<br />";
+    }
+    
+    public function testTwo($one, $two) {
+        echo "{$one}, {$two}<br />";
+    }
+
+    public function testThree($one, $two, $three) {
+        echo "{$one}, {$two}, {$three}<br />";
+    }
+
+    public function testFour($one, $two, $three, $four) {
+        echo "{$one}, {$two}, {$three}, {$four}<br />";
+    }
+
+    public function testFive($one, $two, $three, $four, $five) {
+        echo "{$one}, {$two}, {$three}, {$four}, {$five}<br />";
+    }
 }
